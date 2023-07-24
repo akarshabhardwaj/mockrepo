d8c452a962690c23a4e5aa3c6956074f381b1381
akarshabhardwaj <akarshabhardwaj7280976503@gmail.com>
2023-07-24 14:37:33 +0530

    added

--- controllers/booking.controller.js
+++ controllers/booking.controller.js
@@ -5,11 +5,10 @@ const UserToken = require("../utils/UserToken");
 const BookProperty = async (req, res) => {
     try {
         let userData = UserToken(req);
+        console.log("userdata", userData)
         let Id = userData.userId;
         let name = userData.username;
-        let place = userData.place;
-        let price = userData.price;
-        let { checkIn, checkOut, phone } = req.body
+        let { checkIn, checkOut, phone, price, place } = req.body
         let booking = new BookingModel({ user: Id, place, checkIn, checkOut, name, phone, price })
         booking.save()
         res.send({ "msg": "Booking Successful" });
@@ -33,6 +32,7 @@ const Booked_by_user = async (req, res) => {
         let Id = userData.userId;
         console.log(Id)
         let user_bookings = await BookingModel.find({ user: Id }).populate("place");
+        console.log(user_bookings)
         res.send({ "user": user_bookings })
     } catch (error) {
         res.send({ "error": error.message });
